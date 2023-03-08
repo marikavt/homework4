4a8126ba3455c96dac5ce401e1595a5d9b5d4849
mariami <mariam.kavtaradze978@gmail.com>
2023-03-08 22:51:35 +0400

    add file

---
+++ script.js
@@ -0,0 +1,35 @@
+// const array2 = ["string","valueToReplace","valueToReplaceWith"];
+// console.log(array2);
+// array2.splice(1,1)
+// console.log(array2)
+
+// let array3 = array2.join(' ');
+// console.log(array3);
+
+
+
+////////////////////////////////////////////
+
+
+// const capitalized = function(sentence){
+
+//     let str = sentence.split(" ");
+//     for (let i = 0;i<str.length;i++){
+//         str[i] = str[i][0].toUpperCase() + str[i].substring(1);
+//     }
+//     return str.join(" ");
+// }
+
+// console.log(capitalized("the catcher in the rye"));
+
+
+///////////////////////////////////////
+
+// const customer = [{name:'Mari',age:25},{name:'Dato',age:34},{name:'Saba',age:21},{name:'Amirani',age:32}]
+
+// const age = customer.sort((a,b) => {
+//     return a.age - b.age
+// });
+
+// console.log(age);
+
